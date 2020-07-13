f0af031f851ed01ef3224071cc6cee30b52a0e72
bilbo <fonbeauty@mail.ru>
2020-07-14 00:24:51 +0300

    remake MVN

--- app.js
+++ app.js
@@ -3,14 +3,42 @@
 // let spell = [Раскрошить - Crumble,]
 
 const request1 = new XMLHttpRequest()
+const chronoBlock = document.getElementById('chronomancer-block')
+const elemenеBlock = document.getElementById('elementalist-block')
+const chronoSchool = document.getElementById('chronomancer-school')
+const elementSchool = document.getElementById('elementalist-school')
 
 request1.open('GET', 'https://fonbeauty.github.io/frostgrave/json/chronomancer_spells.json')
 request1.responseType = 'json'
 request1.send()
 request1.onload = function () {
-    const chronomancerSchool = request1.response
-    console.log(chronomancerSchool);
-    showSpells(chronomancerSchool)
+    const chronoSchoolObj = request1.response
+
+    console.log(chronoSchoolObj);
+    // showSpells(chronomancerSchool)
+    showSchool(chronoBlock, chronoSchool, chronoSchoolObj, 'chrono')
+
+}
+
+function showSchool(nodeBlock, nodeSchool, schoolObj, school) {
+    nodeSchool.addEventListener('click', () => {
+        const switcher = document.getElementById(school +'0')
+
+        if (!switcher) {
+            spellsList = document.createElement('ul')
+            nodeBlock.appendChild(spellsList)
+            for (let i = 0; i < schoolObj.spells.length; i++) {
+                const listItem = document.createElement('li')
+                listItem.setAttribute('id', school + i)
+                listItem.setAttribute('class', 'spell')
+                listItem.textContent = schoolObj.spells[i].name
+                spellsList.appendChild(listItem)
+            }
+        } else {
+
+        }
+    })
+
 }
 
 const request2 = new XMLHttpRequest()
@@ -19,9 +47,12 @@ request2.open('GET', 'https://fonbeauty.github.io/frostgrave/json/elementalist_s
 request2.responseType = 'json'
 request2.send()
 request2.onload = function () {
-    const elementalistSchool = request2.response
-    console.log(elementalistSchool);
+    const elementSchoolObj = request2.response
+    console.log(elementSchoolObj);
+    // showSpells(elementalistSchool)
+    showSchool(elemenеBlock, elementSchool, elementSchoolObj, 'element')
 }
+
 
 function showSpells(schoolObject) {
     let schoolBlock = document.createElement('div')
@@ -32,7 +63,7 @@ function showSpells(schoolObject) {
     body.appendChild(schoolBlock)
 
     const spells = schoolObject['spells']
-    for (let i = 0; i < spells.length; i++ ){
+    for (let i = 0; i < spells.length; i++) {
         const spellBlock = document.createElement('div')
         const spellName = document.createElement('h3')
         const spellInnerBlock = document.createElement('div')
